e7b7cda9e51c6949dfbca02447146ec80f095b7f
Eyob1984 <tamireyob@gmail.com>
2019-12-03 14:56:19 -0800

    code201 project

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('we are running the script now');
+      var funTimes = 'this is an alert!';
+      alert(funTimes);
+
+      var nameOfTheUser =prompt ('what is your name?');
+      console.log('The user said their name was ' + nameOfTheUser);
+      alert('Hello, '+ nameOfTheUser  + ' Welcome to my site!');
