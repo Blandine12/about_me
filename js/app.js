25e7f4dab7632bccaff9aca6fa896ac28108b805
Eyob1984 <tamireyob@gmail.com>
2019-12-03 20:14:18 -0800

    css and js created

---
+++ js/app.js
@@ -0,0 +1,61 @@
+'use strict';
+console.log('about me');
+
+var greetings = ('Welcome!');
+alert('greetings');
+var nameOfTheUser =prompt ('What is your name?');
+
+alert('welcome again '+ nameOfTheUser);
+
+var whatYouLove =prompt ('Do you know where Ethiopia is? ');
+
+if(whatYouLove.toLowerCase() === 'yes'|| whatYouLove.toLowerCase() === 'y')
+{
+alert(' Cool!');
+}
+  
+else{
+ alert('Its located in east Africa.');
+}
+var city=prompt('Do you think am originally form Ethiopia?');
+    
+if(city.toLowerCase() === 'yes'||city.toLowerCase() === 'y'){
+alert('you got it!');
+}
+else{
+alert('Am originally for Ethiopia');
+}
+var tribes=prompt('Do you know there are more than 80 tribes in Ethiopia?');
+    
+if(tribes.toLowerCase() === 'yes'||tribes.toLowerCase() === 'y')
+{
+alert('Yes! there are more than 80 tribes!');
+}
+else{
+alert(' There are more than 80 tribes!');
+}
+var months=prompt('Do you know Ethiopia has 13months in year?');
+    
+if(months.toLowerCase() === 'yes'||months.toLowerCase() === 'y')
+{
+alert('Yea!');
+}
+else{
+alert(' Ethiopia AKA called 13months sunshine!');
+}    
+var orgin=prompt('Do you know Red fox only extsisted in Ethiopia?');
+    
+if(origin.toLowerCase() === 'yes'||origin.toLowerCase() === 'y')
+{
+alert('yes!');
+}
+else{
+alert('Red fox is only found in Ethiopia!');
+}
+      
+      
+      
+      // var nameOfHobby =prompt ('Name one sport you like? ');
+      // var bestColor =prompt ('Indoor or outdoor?')
+      // console.log('The user said their name was ');
+      // alert('Hello! '+ nameOfTheUser +','+' Welcome to my site!' +'You live in '+ whereYouLive +', ' + nameOfHobby +' is your sport '+'and ' + ' you like it when you play '+bestColor);
