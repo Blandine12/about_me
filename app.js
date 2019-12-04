6192f00808eaa2808ee778c56088fb250638ee20
Eyob1984 <tamireyob@gmail.com>
2019-12-03 19:31:09 -0800

    modified index.html and app.js

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
-console.log('we are running the script now');
-      var funTimes = 'this is an alert!';
-      alert(funTimes);
+'use strict';
+console.log('about me');
 
-      var nameOfTheUser =prompt ('what is your name?');
-      console.log('The user said their name was ' + nameOfTheUser);
-      alert('Hello, '+ nameOfTheUser  + ' Welcome to my site!');
+var greetings = ('Welcome!');
+alert('greetings');
+var nameOfTheUser =prompt ('What is your name?');
+
+alert('welcome again '+ nameOfTheUser);
+
+var whatYouLove =prompt ('Do you know where Ethiopia is? ');
+
+if(whatYouLove.toLowerCase() === 'yes'|| whatYouLove.toLowerCase() === 'y')
+{
+alert(' Cool!');
+}
+  
+else{
+ alert('Its located in east Africa.');
+}
+var city=prompt('Do you think am originally form Ethiopia?');
+    
+if(city.toLowerCase() === 'yes'||city.toLowerCase() === 'y'){
+alert('you got it!');
+}
+else{
+alert('Am originally for Ethiopia');
+}
+var tribes=prompt('Do you know there are more than 80 tribes in Ethiopia?');
+    
+if(tribes.toLowerCase() === 'yes'||tribes.toLowerCase() === 'y')
+{
+alert('Yes! there are more than 80 tribes!');
+}
+else{
+alert(' There are more than 80 tribes!');
+}
+var months=prompt('Do you know Ethiopia has 13months in year?');
+    
+if(months.toLowerCase() === 'yes'||months.toLowerCase() === 'y')
+{
+alert('Yea!');
+}
+else{
+alert(' Ethiopia AKA called 13months sunshine!');
+}    
+var orgin=prompt('Do you know Red fox only extsisted in Ethiopia?');
+    
+if(origin.toLowerCase() === 'yes'||origin.toLowerCase() === 'y')
+{
+alert('yes!');
+}
+else{
+alert('Red fox is only found in Ethiopia!');
+}
+      
+      
+      
+      // var nameOfHobby =prompt ('Name one sport you like? ');
+      // var bestColor =prompt ('Indoor or outdoor?')
+      // console.log('The user said their name was ');
+      // alert('Hello! '+ nameOfTheUser +','+' Welcome to my site!' +'You live in '+ whereYouLive +', ' + nameOfHobby +' is your sport '+'and ' + ' you like it when you play '+bestColor);
